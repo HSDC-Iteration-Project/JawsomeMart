c058bea9deb76b7d1d6153cb3357ccf40d2be116
Joyce Oh <joyce.oh@live.com>
2024-08-07 12:37:02 -0400

    testing works!!!

---
+++ frontend/test/setup.js
@@ -0,0 +1,10 @@
+import { expect, afterEach } from 'vitest';
+import { cleanup } from '@testing-library/react';
+import * as matchers from "@testing-library/jest-dom/matchers"
+
+// runs a clean after each test case (e.g. clearing jsdom)
+expect.extend(matchers);
+
+afterEach(() => {
+  cleanup();
+});
