86db5fc9b67c0bc71dd2ba73a39b3ceced871d79
Brian <nguyenbrian2323@gmail.com>
2024-08-07 16:48:20 -0700

    checking errors on marketplace render

--- frontend/src/components/Marketplace/Marketplace.jsx
+++ frontend/src/components/Marketplace/Marketplace.jsx
@@ -1,7 +1,7 @@
 /**
  * This file represents a marketplace page on our e-commerce website.
  * This page will render nested components such as listed items, navigation bar, and the shopping cart.
- * 
+ *
  * @returns - The components to be rendered
  * @exports Marketplace - Function to be used by other files
  */
@@ -17,118 +17,134 @@ import './Marketplace.css';
 
 // Defines our Marketplace function to be exported
 const Marketplace = () => {
-    
-    // Creates state array to store Product components
-    const [displayedProducts, setProducts] = useState([]);
-    const [value, setValue] = useState('');
+  // Creates state array to store Product components
+  const [displayedProducts, setProducts] = useState([]);
+  const [value, setValue] = useState('');
 
-			// Calls the getComponents function so we can render the products
-			useEffect(() => {
-				console.log("hit")
-					getComponents();
-			}, []);
+  // Calls the getComponents function so we can render the products
+  useEffect(() => {
+    console.log('hit');
+    getComponents();
+  }, []);
 
-		//holds category type
-		useEffect( () => getSpecificComponents(value), [value])
-    // Creates a new array to hold all products returned from db
-    // const allProducts = [];
+  //holds category type
+  useEffect(() => getSpecificComponents(value), [value]);
+  // Creates a new array to hold all products returned from db
+  // const allProducts = [];
 
-		// filters for categories pressed
-    const getSpecificComponents = (value) => {
-        console.log('value', value);
-			axios.get(`/api/products/${value}`)
-			.then(res => {
-                // console.log('in specific', res.data);
-				// Function that changes the state of products array
-				setProducts(() => {
+  // filters for categories pressed
+  const getSpecificComponents = (value) => {
+    console.log('value', value);
+    axios.get(`/api/products/${value}`).then((res) => {
+      // console.log('in specific', res.data);
+      // Function that changes the state of products array
+      setProducts(() => {
+        // Saves the current array in newProducts
+        const newProducts = [];
+        const arr = res.data.products;
+        // Pushes product components to an array passing in data as props
+        for (let i = 0; i < arr.length; i++) {
+          const newProduct = (
+            <Product
+              product_id={arr[i]._id}
+              id={arr[i].id}
+              title={arr[i].title}
+              price={arr[i].price}
+              category={arr[i].category}
+              description={arr[i].description}
+              image={arr[i].image}
+              rating={arr[i].rating}
+            />
+          );
 
-						// Saves the current array in newProducts
-						const newProducts = [];
-						const arr = res.data.products;
-						// Pushes product components to an array passing in data as props
-						for (let i = 0; i < arr.length; i++) {
-							const newProduct = (<Product
-								product_id={arr[i]._id}
-								id={arr[i].id}
-								title={arr[i].title}
-								price={arr[i].price} 
-								category={arr[i].category}
-								description={arr[i].description}
-								image={arr[i].image}
-								rating={arr[i].rating}
-							/>);
+          // Pushes each product into allProducts array and displayedProducts arr
+          newProducts.push(newProduct);
+          // allProducts.push(newProducts);
+        }
+        console.log('new products', newProducts);
+        return newProducts;
+      });
+    });
+  };
+  // Function that sends a "GET" request to the DB to fetch product data
+  const getComponents = (value) => {
+    console.log('value', value);
+    // Sends a "GET" request for products stored in db
+    axios
+      .get('/api/products')
+      .then((res) => {
+        // Function that changes the state of products array
+        setProducts(() => {
+          // Saves the current array in newProducts
+          const newProducts = [];
+          const arr = res.data;
+          // Pushes product components to an array passing in data as props
+          for (let i = 0; i < arr.length; i++) {
+            const newProduct = (
+              <Product
+                product_id={arr[i]._id}
+                id={arr[i].id}
+                title={arr[i].title}
+                price={arr[i].price}
+                category={arr[i].category}
+                description={arr[i].description}
+                image={arr[i].image}
+                rating={arr[i].rating}
+              />
+            );
 
-							// Pushes each product into allProducts array and displayedProducts arr
-							newProducts.push(newProduct);
-							// allProducts.push(newProducts);
-						}
-                        console.log('new products', newProducts);
-						return newProducts;
-				});
-		})
-    }
-		// Function that sends a "GET" request to the DB to fetch product data
-    const getComponents = (value) => {
-        console.log('value', value);
-        // Sends a "GET" request for products stored in db
-        axios.get('/api/products')
-            .then(res => {
+            // Pushes each product into allProducts array and displayedProducts arr
+            newProducts.push(newProduct);
+            // allProducts.push(newProducts);
+          }
+          return newProducts;
+        });
+      })
 
-                // Function that changes the state of products array
-                setProducts(() => {
+      // Catches any errors during our get request and displays a message box with the error
+      .catch((e) => {
+        alert(e);
+      });
+  };
 
-                    // Saves the current array in newProducts
-                    const newProducts = [];
-                    const arr = res.data;
-                    // Pushes product components to an array passing in data as props
-                    for (let i = 0; i < arr.length; i++) {
-                        const newProduct = (<Product
-                            product_id={arr[i]._id}
-                            id={arr[i].id}
-                            title={arr[i].title}
-                            price={arr[i].price} 
-                            category={arr[i].category}
-                            description={arr[i].description}
-                            image={arr[i].image}
-                            rating={arr[i].rating}
-                        />);
-
-                        // Pushes each product into allProducts array and displayedProducts arr
-                        newProducts.push(newProduct);
-                        // allProducts.push(newProducts);
-                    }
-                    return newProducts;
-                });
-            })
-
-            // Catches any errors during our get request and displays a message box with the error
-            .catch(e => {
-                alert(e);
-            })
-    };
-
-    // Returns a styled div containing the rendered products
-    return (
-        // categories
-        <div className='marketContainer'>
-					<nav>
-							<button value="Men" 
-							onClick={()=> setValue('men\'s clothing')}>Men</button> 
-							<button value="Women" 
-							onClick={() => {setValue('women\'s clothing')}}>Woman</button>					
-                            <button value="Jewelery" 
-							onClick={()=> {setValue('jewelery')}}> Jewelery </button>
-							<button value="Electronics" 
-							onClick={()=> {setValue('electronics')}}>Electronics</button>
-						
-					</nav>
-					<div className='product-display'>
-						{ displayedProducts }
-					</div>
-        </div>
-    );
+  // Returns a styled div containing the rendered products
+  return (
+    // categories
+    <div className='marketContainer'>
+      <nav>
+        <button value='Men' onClick={() => setValue("men's clothing")}>
+          Men
+        </button>
+        <button
+          value='Women'
+          onClick={() => {
+            setValue("women's clothing");
+          }}
+        >
+          Woman
+        </button>
+        <button
+          value='Jewelery'
+          onClick={() => {
+            setValue('jewelery');
+          }}
+        >
+          {' '}
+          Jewelery{' '}
+        </button>
+        <button
+          value='Electronics'
+          onClick={() => {
+            setValue('electronics');
+          }}
+        >
+          Electronics
+        </button>
+      </nav>
+      <div className='product-display'>{displayedProducts}</div>
+    </div>
+  );
 };
-
 
 // {/* <ul className='categories'>
 // <li><button onClick={getFiltered} className='mens'>
