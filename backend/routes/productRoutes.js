cf561ae45b40601d832d39314ba3ccc4f624ac88
Joyce Oh <joyce.oh@live.com>
2024-08-06 21:59:44 -0400

    filtering category works

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const {getProducts} = require('../controllers/productController');
+const {getProducts, getCategory} = require('../controllers/productController');
 
 router.get('/products', getProducts,(req,res)=>{
 
 res.json(res.locals.allProducts)
 
+})
+
+router.get('/products/:category', getCategory, (req, res)=>{
+    res.json(res.locals);
 })
 
 
--- frontend/src/components/Marketplace/Marketplace.jsx
+++ frontend/src/components/Marketplace/Marketplace.jsx
@@ -5,6 +5,7 @@
  * @returns - The components to be rendered
  * @exports Marketplace - Function to be used by other files
  */
+//import search bar
 
 // Importing necessary tools
 import { useState, useEffect } from 'react';
@@ -19,13 +20,55 @@ const Marketplace = () => {
     
     // Creates state array to store Product components
     const [displayedProducts, setProducts] = useState([]);
+    const [value, setValue] = useState('');
 
+			// Calls the getComponents function so we can render the products
+			useEffect(() => {
+				console.log("hit")
+					getComponents();
+			}, []);
+
+		//holds category type
+		useEffect( () => getSpecificComponents(value), [value])
     // Creates a new array to hold all products returned from db
     // const allProducts = [];
 
-    // Function that sends a "GET" request to the DB to fetch product data
-    const getComponents = () => {
+		// filters for categories pressed
+    const getSpecificComponents = (value) => {
+        console.log('value', value);
+			axios.get(`/api/products/${value}`)
+			.then(res => {
 
+				// Function that changes the state of products array
+				setProducts(() => {
+
+						// Saves the current array in newProducts
+						const newProducts = [];
+						const arr = res.data;
+						// Pushes product components to an array passing in data as props
+						for (let i = 0; i < arr.length; i++) {
+								const newProduct = (<Product
+										product_id={arr[i]._id}
+										id={arr[i].id}
+										title={arr[i].title}
+										price={arr[i].price} 
+										category={arr[i].category}
+										description={arr[i].description}
+										image={arr[i].image}
+										rating={arr[i].rating}
+								/>);
+
+								// Pushes each product into allProducts array and displayedProducts arr
+								newProducts.push(newProduct);
+								// allProducts.push(newProducts);
+						}
+						return newProducts;
+				});
+		})
+    }
+		// Function that sends a "GET" request to the DB to fetch product data
+    const getComponents = (value) => {
+        console.log('value', value);
         // Sends a "GET" request for products stored in db
         axios.get('/api/products')
             .then(res => {
@@ -63,19 +106,43 @@ const Marketplace = () => {
             })
     };
 
-    // Calls the getComponents function so we can render the products
-    useEffect(() => {
-        getComponents();
-        console.log("hit")
-    }, []);
-
     // Returns a styled div containing the rendered products
     return (
-        <div className="product-display">
-            { displayedProducts }
+        // categories
+        <div className='marketContainer'>
+					<nav>
+							<button value="Men" 
+							onClick={()=> setValue('men\'s clothing')}>Men</button> 
+							<button value="Women" 
+							onClick={() => {setValue('women\'s clothing')}}>Woman</button>					
+                            <button value="Jewelery" 
+							onClick={()=> {setValue('jewelery')}}> Jewelery </button>
+							<button value="Electronics" 
+							onClick={()=> {setValue('electronics')}}>Electronics</button>
+						
+					</nav>
+					<div className='product-display'>
+						{ displayedProducts }
+					</div>
         </div>
     );
 };
+
+
+// {/* <ul className='categories'>
+// <li><button onClick={getFiltered} className='mens'>
+// 	Men&apos;s
+// </button></li>
+// <li><button className='womens'>
+// 	Women&apos;s
+// </button></li>
+// <li><button className='jewlery'>
+// 	Jewlery
+// </button></li>
+// <li><button className='electronics'>
+// 	Electronics
+// </button></li>
+// </ul> */}
 
 // Exports the Marketplace function
 export default Marketplace;
