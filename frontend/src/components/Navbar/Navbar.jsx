2c0d7d02b0eb6150407a6bbde77ce29d286d26a6
Brian <nguyenbrian2323@gmail.com>
2024-08-08 14:54:56 -0700

    nav bar icons

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -7,9 +7,14 @@ Imports
 import { Link } from 'react-router-dom';
 import { AuthedUserContext } from '../../App';
 import { useContext, useEffect, useState } from 'react';
-import styles from "./Navbar.module.css";
+import styles from './Navbar.module.css';
 import logo from '../../assets/icons8-shop-96.png'; // Import the image
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faShoppingCart,
+  faSignOut,
+  faStore,
+} from '@fortawesome/free-solid-svg-icons';
 
 /* =======================================================
 Helper Functions
@@ -19,21 +24,21 @@ Helper Functions
 Component
 =======================================================*/
 
-function Navbar({handleSignout}) {
+function Navbar({ handleSignout }) {
   /*--------------- States/Hooks ---------------*/
   const user = useContext(AuthedUserContext);
 
   const toggleTheme = () => {
-    console.log('Toggle Theme Called')
-    setIsDarkMode(prevMode => !prevMode);
-  }
+    console.log('Toggle Theme Called');
+    setIsDarkMode((prevMode) => !prevMode);
+  };
 
-  const[isDarkMode, setIsDarkMode] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [cartItemCount, setCartItemCount] = useState(0);
 
   useEffect(() => {
-    document.body.classList.toggle('darkmode', isDarkMode)
+    document.body.classList.toggle('darkmode', isDarkMode);
   }, [isDarkMode]);
-
 
   /*--------------- Return ---------------*/
 
@@ -43,37 +48,64 @@ function Navbar({handleSignout}) {
         {/* <h3>nav bar</h3> */}
         <div className={styles.navContainer}>
           <div className={styles.navLeft}>
-            <a href="/marketplace">
-            <img src={logo} alt="LOGO"/>
+            <a href='/marketplace'>
+              <img src={logo} alt='LOGO' />
             </a>
           </div>
           {user ? (
-          <div className={styles.navRight}>          
-               
-              <button id = "theme-switch" onClick={toggleTheme}>
-                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Z"/></svg>
-                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M480-280q-83 0-141.5-58.5T280-480q0-83 58.5-141.5T480-680q83 0 141.5 58.5T680-480q0 83-58.5 141.5T480-280ZM200-440H40v-80h160v80Zm720 0H760v-80h160v80ZM440-760v-160h80v160h-80Zm0 720v-160h80v160h-80ZM256-650l-101-97 57-59 96 100-52 56Zm492 496-97-101 53-55 101 97-57 59Zm-98-550 97-101 59 57-100 96-56-52ZM154-212l101-97 55 53-97 101-59-57Z"/></svg>
+            <div className={styles.navRight}>
+              <button id='theme-switch' onClick={toggleTheme}>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  height='24px'
+                  viewBox='0 -960 960 960'
+                  width='24px'
+                  fill='#e8eaed'
+                >
+                  <path d='M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Z' />
+                </svg>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  height='24px'
+                  viewBox='0 -960 960 960'
+                  width='24px'
+                  fill='#e8eaed'
+                >
+                  <path d='M480-280q-83 0-141.5-58.5T280-480q0-83 58.5-141.5T480-680q83 0 141.5 58.5T680-480q0 83-58.5 141.5T480-280ZM200-440H40v-80h160v80Zm720 0H760v-80h160v80ZM440-760v-160h80v160h-80Zm0 720v-160h80v160h-80ZM256-650l-101-97 57-59 96 100-52 56Zm492 496-97-101 53-55 101 97-57 59Zm-98-550 97-101 59 57-100 96-56-52ZM154-212l101-97 55 53-97 101-59-57Z' />
+                </svg>
               </button>
-              
-              <li><Link to='/cart'>My Cart</Link></li>
-              <li><Link to="/Marketplace">Marketplace</Link></li>
-              <li><Link to='' onClick={handleSignout}>Sign Out</Link></li>
-          
-          </div>
-          ) : (
-          <div className={styles.navRight}>          
-              <li><Link to="/">Log In</Link></li>
-              <li><Link to="/signup">Sign Up</Link></li>  
-          </div>
-          )}
 
-       </div>
+              <li>
+                <Link to='/cart' className={styles.cartLink}>
+                  My Cart
+                </Link>
+                <FontAwesomeIcon icon={faShoppingCart} />
+              </li>
+              <li>
+                <Link to='/Marketplace'>Marketplace</Link>
+                <FontAwesomeIcon icon={faStore} />
+              </li>
+              <li>
+                <Link to='' onClick={handleSignout}>
+                  Sign Out
+                </Link>
+                <FontAwesomeIcon icon={faSignOut} />
+              </li>
+            </div>
+          ) : (
+            <div className={styles.navRight}>
+              <li>
+                <Link to='/'>Log In</Link>
+              </li>
+              <li>
+                <Link to='/signup'>Sign Up</Link>
+              </li>
+            </div>
+          )}
+        </div>
       </nav>
-      
     </>
   );
 }
 
 export default Navbar;
-
-
